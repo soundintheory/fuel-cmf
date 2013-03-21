9655100bdacb913a77ecbfe540ec1d2e094bda67
Andy White <andy@soundintheory.co.uk>
2013-03-21 07:36:48 +0000

    Fixed the way the auto cache driver finds loaded model classes - it was missing ones that were queried using an array-based result method

--- classes/Cache/Driver/Auto.php
+++ classes/Cache/Driver/Auto.php
@@ -61,10 +61,7 @@ class Auto extends Simple {
 	protected function startListeners()
 	{
 		$this->logger = new \CMF\Doctrine\QueryLogger();
-		$this->listener = new \CMF\Doctrine\CacheListener();
-		
 		\DoctrineFuel::manager()->getConnection()->getConfiguration()->setSQLLogger($this->logger);
-		\DoctrineFuel::manager()->getEventManager()->addEventSubscriber($this->listener);
 	}
 	
 	public function set($response)
@@ -76,7 +73,7 @@ class Auto extends Simple {
 		$driver = $this;
 		
 		// We need to add any file dependencies for the view now, because it will be a string later on
-		if (!is_null($view)) $this->addFilesForClass(get_class($view));
+		//if (!is_null($view)) $this->addFilesForClass(get_class($view));
 		
 		\Event::register('shutdown', function() use($driver) {
 			$driver->shutdown();
@@ -98,13 +95,18 @@ class Auto extends Simple {
 			$this->files = array_unique(array_merge($this->files, $templates));
 		}
 		
-		// Add files relating to the controller
-		if (!is_null($controller)) $this->addFilesForClass(get_class($controller));
+		// Add all loaded files within the app root, excluding cache and model proxies
+		$this->files = array_merge($this->files, array_filter(get_included_files(), function($path) {
+			return strpos($path, APPPATH) === 0 && strpos($path, APPPATH.'cache') !== 0 && strpos($path, APPPATH.'classes/proxy') !== 0;
+		}));
 		
-		// Add files relating to models
+		$model_classes = array_filter(get_declared_classes(), function($class) {
+			return strpos($class, 'Model_') === 0;
+		});
+		
+		// Construct an allowed list of tables for the cache queries
 		$em = \DoctrineFuel::manager();
-		foreach ($this->listener->classes as $model) {
-			$this->addFilesForClass($model);
+		foreach ($model_classes as $model) {
 			$meta = $em->getClassMetadata($model);
 			if ($meta->isMappedSuperclass || !isset($meta->columnNames['updated_at']) || $meta->rootEntityName != $meta->name) continue;
 			if (!in_array($meta->table['name'], $tables)) {
@@ -112,27 +114,20 @@ class Auto extends Simple {
 			}
 		}
 		
-		// Filter out any files not in the app path
-		$this->files = array_values(array_filter($this->files, function($path) {
-			return strpos($path, APPPATH) === 0;
-		}));
-		
-		// Add any loaded config files
-		$this->files = array_merge($this->files, array_filter(get_included_files(), function($path) {
-			return strpos($path, APPPATH.'config/') === 0;
-		}));
-		
-		// Replace the app path each file path
+		// Remove the app path from each file path, making them relative
 		$this->files = array_map(function($path) {
 			return str_replace(APPPATH, '', $path);
 		}, $this->files);
 		
 		// Construct ourselves a number of sub queries to check all the relevant records in the database
-		foreach ($queries as $num => $query) {
+		$num = 0;
+		foreach ($queries as $query) {
 			
 			$parser = new \PHPSQLParser();
 			$parsed = $parser->parse($query, true);
-			if (!isset($parsed['FROM']) || count($parsed['FROM']) === 0) continue;
+			if (!isset($parsed['FROM']) || count($parsed['FROM']) === 0) {
+				continue;
+			}
 			
 			$aliases = array();
 			foreach ($parsed['FROM'] as $part) {
@@ -158,11 +153,11 @@ class Auto extends Simple {
 			
 			$subqueries[] = 'q'.$num;
 			$sql .= ($num > 0 ? ',' : '').' (SELECT '.(count($aliases) > 1 ? 'GREATEST(' : '').'IFNULL(MAX('.implode('.updated_at),0), IFNULL(MAX(', $aliases).'.updated_at),0)'.(count($aliases) > 1 ? ')' : '').' AS updated_at'.$append.') q'.$num;
+			$num++;
 			
 		}
 		
 		if (!empty($sql)) {
-			
 			// Complete the mega query that will check if items are updated or not...
 			if (count($subqueries) > 1) {
 				$sql = 'SELECT GREATEST('.implode('.updated_at, ', $subqueries).'.updated_at) AS updated_at, ('.implode('.count+', $subqueries).'.count) AS count FROM'.$sql;
@@ -187,18 +182,6 @@ class Auto extends Simple {
 		// serialize and write it to disk
 		\CMF\Cache::writeCacheFile($this->path, serialize($result));
 		
-	}
-	
-	protected function addFilesForClass($class)
-	{
-		$object = new \ReflectionClass($class);
-		
-		while ($object !== false) {
-			$filename = $object->getFileName();
-			if (strpos($filename, APPPATH) !== 0) break;
-			if (!in_array($filename, $this->files)) $this->files[] = $filename;
-			$object = $object->getParentClass();
-		}
 	}
 	
 }
