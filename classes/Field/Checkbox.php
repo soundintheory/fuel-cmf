841e18ad6905952e2a18cfe86ae55c088db89721
Nick Martin <nick@soundintheory.co.uk>
2013-06-05 13:33:39 +0100

    checkbox now has 'description' capability for adding extra information to the field.

--- classes/Field/Checkbox.php
+++ classes/Field/Checkbox.php
@@ -21,10 +21,12 @@ class Checkbox extends Base {
     	$input = \Form::checkbox($settings['mapping']['fieldName'], '1', $value, $input_attributes);
     	$title = $settings['title'].($has_errors ? ' - '.$errors[0] : '');
     	$label = (!$include_label) ? $input : html_tag('label', array( 'class' => 'checkbox' ), $input.'<span class="item-label"> '.$title.'</span>');
-        
+
+        $description = isset($settings['description']) ? "<span class=\"help-block\">".$settings['description']."</span>" : "";
+
         if (isset($settings['wrap']) && $settings['wrap'] === false) return $label;
     	
-        return html_tag('div', array( 'class' => 'field-type-checkbox controls control-group'.($has_errors ? ' error' : '') ), $off_input.$label);
+        return html_tag('div', array( 'class' => 'field-type-checkbox controls control-group'.($has_errors ? ' error' : '') ), $off_input.$label.$description);
     }
 	
 }
