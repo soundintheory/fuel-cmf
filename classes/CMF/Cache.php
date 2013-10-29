275e4c9018a764ec7c618bcc7f7912b2334bbd99
Andy White <andy@soundintheory.co.uk>
2013-10-29 14:52:23 +0000

    Cache ignores jquery's ajax 'no cache' param

--- classes/CMF/Cache.php
+++ classes/CMF/Cache.php
@@ -29,6 +29,9 @@ class Cache {
 		
 		// Check for excluded URLS
 		$uri = '/'.str_replace(array('?debug', '&debug'), '', trim($_SERVER['REQUEST_URI'], '/'));
+        $jq = \Input::get('_', null);
+        if ($jq !== null) $uri = str_replace(array("?_=$jq", "&_=$jq"), '', $uri);
+        
 		$excluded_urls = $config['excluded_urls'];
 		foreach ($excluded_urls as $url) {
 			if (strpos($url, '*') !== false && strpos($uri.'/', str_replace('*', '', $url)) === 0) {
