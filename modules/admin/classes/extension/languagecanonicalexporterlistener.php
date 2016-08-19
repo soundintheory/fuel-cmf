834c8dbd90d8abd26bddbce1c1d3a7f1bde25b24
Yann Tripet <yann@soundintheory.co.uk>
2016-08-19 08:59:50 +0100

    'added sender and receiver for canonical language'

--- modules/admin/classes/extension/languagecanonicalexporterlistener.php
+++ modules/admin/classes/extension/languagecanonicalexporterlistener.php
@@ -8,43 +8,82 @@ use Doctrine\ORM\Event\OnFlushEventArgs,
 class Extension_Languagecanonicalexporterlistener implements EventSubscriber
 {
     private $jsonObject;
+    private $toProcessPostFush;
+    private $toProcessPostFushDelete;
 
 	public function getSubscribedEvents()
     {
         return array(
-            'onFlush'
+            'onFlush',
+            'postFlush'
         );
     }
 
     public function onFlush(OnFlushEventArgs $args)
     {
+        $this->toProcessPostFush = array();
+        $this->toProcessPostFushDelete = array();
+
         $em = $args->getEntityManager();
         $uow = $em->getUnitOfWork();
 
         // Process the aliases after the other stuff
+        $x = 0;
         foreach ($uow->getScheduledEntityInsertions() AS $entity) {
-            if(!empty($entity->settings) && !isset($entity->settings['original_id']) && $entity->settings['original_id'] > 0)
-                $this->processItem($entity, $em);
+            if(!empty($entity->settings) && !isset($entity->settings['original_id']) && $entity->settings['original_id'] > 0){
+                $this->toProcessPostFush[$x] = new \stdClass();
+                $this->toProcessPostFush[$x]->id  = $entity->id;
+                $this->toProcessPostFush[$x]->class  = get_class($entity);
+                $x++;
+            }
         }
 
         foreach ($uow->getScheduledEntityUpdates() AS $entity) {
             $metadata = $em->getClassMetadata(get_class($entity));
             //if change of Url
             if ($metadata->name == 'CMF\\Model\\URL') {
-                $this->processUrlItem($entity, $em);
+                $this->toProcessPostFush[$x] = new \stdClass();
+                $this->toProcessPostFush[$x]->id  = $entity->item_id;
+                $this->toProcessPostFush[$x]->class  = $entity->type;
+                $x++;
             }
+        }
+        $y = 0;
+        foreach ($uow->getScheduledEntityDeletions() AS $entity) {
+            $metadata = $em->getClassMetadata(get_class($entity));
+            //if change of Url
+            if ($metadata->name == 'CMF\\Model\\URL') {
+                $this->toProcessPostFushDelete[$x] = new \stdClass();
+                $this->toProcessPostFushDelete[$x]->id  = $entity->item_id;
+                $this->toProcessPostFushDelete[$x]->class  = $entity->type;
+                $y++;
+            }
+        }
+
+
+    }
+
+    public function postFlush(PostFlushEventArgs $args)
+    {
+        foreach($this->toProcessPostFush as $anItem)
+        {
+            $className = $anItem->class;
+            $item = $className::find($anItem->id);
+            $this->processItem($item);
+        }
+
+        foreach($this->toProcessPostFushDelete as $anItem)
+        {
+            $className = $anItem->class;
+            $item = $className::find($anItem->id);
+            $this->processItem($item,true);
         }
 
         $this->exportLanguageCanonical();
     }
 
-    protected function processUrlItem(&$entity, &$em){
-        $modelClass = $entity->type;
-        $model = $modelClass::find($entity->item_id);
-        $this->processItem($model,$em);
-    }
 
-    protected function processItem(&$entity, &$em){
+    protected function processItem(&$entity,$delete = false){
         $tableName = \Admin::getTableForClass(get_class($entity));
         if(empty($this->jsonObject))
         {
@@ -53,7 +92,7 @@ class Extension_Languagecanonicalexporterlistener implements EventSubscriber
         }
         if(empty($this->jsonObject->data->{$tableName}))
             $this->jsonObject->data->{$tableName} = array();
-        $object = $entity->jsonLanguageDataObject();
+        $object = $entity->jsonLanguageDataObject($delete);
         if(!in_array($object,$this->jsonObject->data->{$tableName}))
             $this->jsonObject->data->{$tableName}[] = $object;
     }
@@ -66,5 +105,7 @@ class Extension_Languagecanonicalexporterlistener implements EventSubscriber
         $curl->set_header('Content-Type', 'application/json');
         $curl->set_params(json_encode($this->jsonObject));
         $curl->execute();
+        /*var_dump($curl->response());
+        exit();*/
     }
 }
